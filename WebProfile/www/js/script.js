32d1a259d400eba222cbc8d7274944be626abffa
mramdniii <142660561+mramdniii@users.noreply.github.com>
2023-11-07 14:44:04 +0700

    Delete WebProfile directory

--- WebProfile/www/js/script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function () {
-    const header = document.querySelector("header.navbar-container");
-    header.classList.toggle("scroll", window.scrollY > 0);
-  });
